1c7b9945be7a776c961790c751da1a4217c09b52
shboulz <shboul89@gmail.com>
2021-03-09 14:01:23 -0500

    Add project files.

---
+++ ReferenceTypes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReferenceTypes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReferenceTypes")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba926eaf-329b-42f4-af30-acf90aac7be3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReferenceTypes/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ReferenceTypes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Strings()
+        {
+            //string name;
+            
+            //string declared;
+            //declared = "This is initialized.";
+
+            //string declaredAndInitialized = "This is both declaring and initializing";
+
+            string firstName = "Addey";
+            string lastName = "Al-Shboul";
+
+            string concatenatedFullName = firstName + " " + lastName;
+
+            string interpolationFullName = $"{firstName} {lastName}";
+
+            string compositeFullName = string.Format("{0} {1}", firstName, lastName);
+
+            Console.WriteLine(firstName);
+
+            Console.WriteLine(lastName);
+
+            Console.WriteLine(concatenatedFullName);
+
+            Console.WriteLine(interpolationFullName);
+
+            Console.WriteLine(compositeFullName);
+           
+        }
+        [TestMethod]
+        public void Collections()
+        {
+            // StringArrays[] 
+            string stringExample = "Hello World";
+
+            string[] stringArray = { "Hello", "World!", "Why", "is it", "always", stringExample };
+
+            string thirdItem = stringArray[2];
+            Console.WriteLine(thirdItem);
+
+            stringArray[0] = "Hey There";
+            Console.WriteLine(stringArray[0]);
+
+            // Lists CTRL-. to fix errors
+            List<string> listOfStrings = new List<string>();
+            List<int> listOfInts = new List<int>();
+
+            listOfStrings.Add("First string for our list");
+            listOfInts.Add(123456);
+
+            Console.WriteLine(listOfInts[0]);
+
+            // Queue
+            Queue<string> firstInFirstOut = new Queue<string>();
+
+            firstInFirstOut.Enqueue("I'm First");
+            firstInFirstOut.Enqueue("I'm Next");
+
+            string firstItem = firstInFirstOut.Dequeue();
+            Console.WriteLine(firstItem);
+
+            // Dictionary
+            Dictionary<int, string> keyAndValue = new Dictionary<int, string>();
+
+            keyAndValue.Add(7, "Agent");
+
+            string valueSeven = keyAndValue[7];
+            Console.WriteLine(valueSeven);
+
+            // Sorted Lists (lookup later)
+            SortedList<int, string> sortedKeyAndValue = new SortedList<int, string>();
+
+            // HashSet
+            HashSet<int> uniqueList = new HashSet<int>();
+
+            // Stack
+            Stack<string> lastInFirstOut = new Stack<string>();
+        }
+        [TestMethod]
+        public void Classes()
+        {
+            Random rng = new Random();
+
+            int randomNumber = rng.Next();
+
+            Console.WriteLine(randomNumber);
+        }
+    }
+}
